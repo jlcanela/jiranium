707076c4eebc423a1bdf75b314a3cc1a28ff6401
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-05 15:26:22 +0200

    Add Query trait and object

---
+++ play/src/main/scala/Query.scala
@@ -0,0 +1,28 @@
+package jiranium.play
+
+import play.api.db._
+import anorm._
+import anorm.SqlParser._
+import java.sql.Connection
+
+object Query extends Query
+
+trait Query {
+
+  // Does not insert anything.
+  // Instead, returns a Sql object with an execute()
+  // method that performs the insert.
+  // usage:
+  // Query.insert("my_table")('foo -> "omg", 'bar -> "ponies!!).execute()
+  def insert(tableName: String)(args: (Any, ParameterValue[_])*): Sql = {
+    val fields = args map {
+      case (s: Symbol, _) ⇒ s.name
+      case (k, _)         ⇒ k.toString
+    }
+    SQL("""insert into %s (%s) values (%s)""".format(
+      tableName,
+      fields mkString ", ",
+      fields map ("{"+_+"}") mkString ", "
+    )).on(args: _*)
+  }
+}
