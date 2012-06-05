e14fa082ad7ff3aa63e2954719af7c9e911c4370
Thibault Duplessis <thibault.duplessis@gmail.com>
2012-06-05 14:51:53 +0200

    Add deployment configuration and documentation

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ jiranium
 ========
 
 A set of reusable components we use in our scala programs
+
+how to publish to jirafe/mvn-repo
+---------------------------------
+
+This section is intended to Jirafe developers.
+
+    sbt publish
+    cd /path/to/mvn-repo
+    cp -r ~/.m2/repository/snapshots/com/jirafe/jiranium-play_2.9.1/* snapshots/com/jirafe/jiranium-play_2.9.1/
+    git commit -m "publish jiranium"
+    git push
--- project/Build.scala
+++ project/Build.scala
@@ -2,16 +2,23 @@ import sbt._
 import Keys._
 
 object JiraniumBuild extends Build {
-  
+
   lazy val play = Project("jiranium-play", file("play")) settings (
     organization := "com.jirafe",
     name := "jiranium-play",
-    version := "1.0",
+    version := "1.0-SNAPSHOT",
     scalaVersion := "2.9.1",
     libraryDependencies ++= Seq(
       "play" %% "play" % "2.0.1",
-      "org.scalaz" %% "scalaz-core" % "6.0.4"
-    ),
-    scalacOptions := Seq("-deprecation", "-unchecked")
+      "org.scalaz" %% "scalaz-core" % "6.0.4"),
+      scalacOptions := Seq("-deprecation", "-unchecked"),
+      publishTo <<= version { (v: String) ⇒
+        val base = Path.userHome.absolutePath + "/.m2/repository"
+        val local = if (v.trim.endsWith("SNAPSHOT"))
+          base + "/snapshots"
+        else
+          base + "/releases"
+        Some(Resolver.file("file", new File(local)))
+      }
   )
 }
